0266519602e04b4c71a8bee663b8f0e93723cf14
Loren Barrick <lorenbarrick@gmail.com>
2015-11-25 17:59:37 -0800

    refactor code, add script tag

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 var heightForm = document.getElementById('height');
 var weightForm = document.getElementById('weight');
 var bmiForm = document.getElementById('bmi');
-var heightValue;
-var weightValue;
 
 function getHeight(event) {
 	event.preventDefault();
-    heightValue = document.getElementById('height-data').value;
-    heightValue = Number(heightValue);
-    alert(heightValue);
-    heightForm.reset();
+  heightValue = document.getElementById('height-data').value;
+  heightValue = Number(heightValue);
+  heightForm.reset();
 }
 
 function getWeight(event) {
-    event.preventDefault();
+  event.preventDefault();
 	weightValue = document.getElementById('weight-data').value;
-    weightValue = Number(weightValue);
-    alert(weightValue);
+  weightValue = Number(weightValue);
 	weightForm.reset();
 }
-function calculateBMI(event) {
-    event.preventDefault();
-    alert('hello');
-	var BMI = weightValue / (heightValue * heightValue) * 703;
-    document.getElementById('bmiList').innerHTML += "<li>" + BMI + "</li>";
 
+function calculateBMI(event) {
+  event.preventDefault();
+	var BMI = weightValue / (heightValue * heightValue) * 703;
+  document.getElementById('bmiList').innerHTML += "<li>" + BMI + "</li>";
 }
+
 heightForm.addEventListener('submit', getHeight, false);
 weightForm.addEventListener('submit', getWeight, false);
 bmiForm.addEventListener('submit', calculateBMI, false);
